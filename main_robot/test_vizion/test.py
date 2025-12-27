8c697206a9add784fbe32085448d077f4c04fa31
Aypimann <101941299+Aypimann@users.noreply.github.com>
2025-12-27 03:55:46 +0100

    bug fix on linkage between detect_tag.py and find_tag_3D.py

--- main_robot/test_vizion/test.py
+++ main_robot/test_vizion/test.py
@@ -4,9 +4,9 @@ import numpy as np
 
 arcuo_size = .1
 
-path = "/home/aypiman/Documents/dev/robot 1A/test_vizion/im0.jpg"
+path = "/home/aypiman/Documents/dev/robot 1A/main_robot/test_vizion/im0.jpg"
 
-pixel_per_meter_at_one_meter = 2268*0.739
+pixel_per_meter_at_one_meter = 2268*0.739*1.623268
 center = (2268/2, 4032/2)
 
 
@@ -24,7 +24,26 @@ X_c = [
     for x, y in detect_tag.tags[0][0][0]
 ]
 
+X_c = [X_c[i] for i in (0, 1, 3, 2)]
+
+for PX in X_c:
+    print(PX)
+
+print("\n","\n")
+
 PX = [*find_tag_3D.compute_tag_points(*X_c, arcuo_size)]
+
+PX = [PX[i] for i in (0, 1, 3, 2)]
+
+d = [
+    np.dot(
+        PX[i] - PX[(i+1)%4],
+        PX[(i+0)%4] - PX[(i+1)%4]
+        )**0.5 
+    for i in range(4)
+]
+
+print(d,"\n")
 
 for PX in PX:
     print(PX)
