ad3812c824d46ae514e9ca529ed254d03f8ba9a5
mentech mentech <mail.lucas2005@gmail.com>
2026-05-14 15:49:26 +0200

    too far

--- main_robot/mvmt/mvmt.ino
+++ main_robot/mvmt/mvmt.ino
@@ -3,9 +3,11 @@
 #include <Arduino.h>
 
 MovementHandler hdl = MovementHandler();
-
+int coter=1;
 void setup() {
   Serial.begin(115200);
+  pinMode(22,INPUT_PULLUP);
+  coter=digitalRead(22)==0? 1:-1;
   setup_pince();
   while (!Serial)
     ;
@@ -13,21 +15,25 @@ void setup() {
 
 bool done = false;
 void loop() {
+  
   if (!done) {
     done = true;
-    //move_pince(1000);
-
-    Serial.printf("%f %f %f\r\n", hdl.posX_, hdl.posY_, hdl.angle_ac);
-    hdl.goTo(10.0, 10.0, false);
-    Serial.printf("%f %f %f\r\n", hdl.posX_, hdl.posY_, hdl.angle_ac);
-    hdl.rotateTo(90.0, false);
-    Serial.printf("%f %f %f\r\n", hdl.posX_, hdl.posY_, hdl.angle_ac);
-    hdl.goTo(20.0, 10.0, false);
-    Serial.printf("%f %f %f\r\n", hdl.posX_, hdl.posY_, hdl.angle_ac);
-    hdl.goTo(0.0, 0.0, false);
-    Serial.printf("%f %f %f\r\n", hdl.posX_, hdl.posY_, hdl.angle_ac);
-    hdl.rotateTo(0.0, false);
-    Serial.printf("%f %f %f\r\n", hdl.posX_, hdl.posY_, hdl.angle_ac);
+    hdl.moveDist(92.3);
+    hdl.rotateTo(-90*coter, false);
+    hdl.moveDist(19);
+    hdl.rotateTo(0, false);
+    hdl.moveDist(-92.325+25);
+    hdl.moveDist(10);
+    hdl.rotateTo(22*coter, false);
+    hdl.moveDist(122);
+    hdl.rotateTo(90*coter, false);
+    hdl.moveDist(50);
+    //retour
+    hdl.moveDist(-45);
+    hdl.rotateTo(23*coter, false);
+    hdl.moveDist(-120);
+    hdl.rotateTo(0, false);
+    hdl.moveDist(-40);
   }
   
   hdl.process();
--- main_robot/mvmt/mvmthdl.ino
+++ main_robot/mvmt/mvmthdl.ino
@@ -47,6 +47,8 @@ void MovementHandler::rotateSteps(int32_t steps) {
 void MovementHandler::moveDist(float dist) {
   moves_.push_back({Move::Translation, dist});
   moveSteps(distToSteps(dist));
+  posX_+=std::cosf(angle_ac)*dist;
+  posX_+=std::sinf(angle_ac)*dist;
 }
 
 void MovementHandler::rotate(float angle) {
